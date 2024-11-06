28e064c524727cd41710b7ee51c55474c9a8a781
Veri Irawan <veriirawan235@gmail.com>
2024-11-06 15:08:17 +0700

    selesai galeri

--- resources/views/galeris/details.blade.php
+++ resources/views/galeris/details.blade.php
@@ -1,41 +1,84 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="card card-solid mt-2">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-12 col-sm-6">
-                <div class="col-12">
-                    <img src="{{ asset($gallery->image_path) }}" class="product-image" alt="{{ $gallery->title }}">
+<div class="container mt-4">
+    <div class="card shadow-lg">
+        <div class="card-header">
+            <h3 class="card-title">Detail Galeri</h3>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12 text-center mb-4">
+                    <img src="{{ asset($gallery->image_path) }}" class="img-fluid rounded" alt="{{ $gallery->title }}" style="max-height: 500px;">
                 </div>
             </div>
-            <div class="col-12 col-sm-6">
-                <h3 class="my-3" style="font-size: 22px">{{ $gallery->title }}</h3>
-                <table style="font-size: 16px">
-                    <tr>
-                        <th>Fotografer</th>
-                        <td>:</td>
-                        <td>{{ $gallery->author }}</td>
-                    </tr>
-                    <tr>
-                        <th>Tanggal Diambil</th>
-                        <td>:</td>
-                        <td>{{ date('d F Y', strtotime($gallery->date_taken)) }}</td>
-                    </tr>
-                    <tr>
-                        <th>Lokasi</th>
-                        <td>:</td>
-                        <td>{{ $gallery->location }}</td>
-                    </tr>
-                    <tr>
-                        <th>Deskripsi</th>
-                        <td>:</td>
-                        <td></td>
-                    </tr>
-                </table>
-                <p>{!! $gallery->description !!}</p>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Deskripsi</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="mt-3">
+                                <p>{!! nl2br(html_entity_decode($gallery->description)) !!}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Informasi Galeri</h5>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th>Judul</th>
+                                        <td>{{ $gallery->title }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Fotografer</th>
+                                        <td>{{ $gallery->author }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Diambil Tanggal</th>
+                                        <td>{{ date('d F Y', strtotime($gallery->date_taken)) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Lokasi</th>
+                                        <td>{{ $gallery->location }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="card-footer">
+            <a href="{{ route('galeri.index') }}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i> Kembali ke Galeri</a>
         </div>
     </div>
 </div>
+@endsection
+
+@section('styles')
+<style>
+    .card-header {
+        background-color: #f8f9fa;
+    }
+    .table th {
+        width: 40%;
+        background-color: #f8f9fa;
+    }
+    .table td {
+        vertical-align: middle;
+    }
+    .card-body p {
+        font-size: 15px;
+        color: #333;
+        line-height: 1.6;
+    }
+</style>
 @endsection
