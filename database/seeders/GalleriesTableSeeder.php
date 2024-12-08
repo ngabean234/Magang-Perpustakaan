35691996b65e876b0871d84eec652cc45a2b012d
Veri Irawan <veriirawan235@gmail.com>
2024-12-08 08:00:46 +0700

    baru2

--- database/seeders/GalleriesTableSeeder.php
+++ database/seeders/GalleriesTableSeeder.php
@@ -17,22 +17,32 @@ class GalleriesTableSeeder extends Seeder
     {
         DB::table('galleries')->insert([
             [
-                'title' => 'Judul Foto 1',
+                'title' => 'Foto 1',
                 'description' => 'Deskripsi foto 1',
-                'image_path' => 'path/to/default/image.jpg',
-                'author' => 'Nama Penulis 1',
+                'image_path' => 'path/to/default/1.jpg',
+                'author' => 'Penulis 1',
                 'date_taken' => '2024-11-01', // Tanggal dalam format YYYY-MM-DD
                 'location' => 'Lokasi Foto 1',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'title' => 'Judul Foto 2',
-                'description' => 'Deskripsi foto 1', // Deskripsi bisa null
-                'image_path' => 'path/to/default/image2.jpg',
-                'author' => 'Nama Penulis 2',
+                'title' => 'Foto 2',
+                'description' => 'Deskripsi foto 2', // Deskripsi bisa null
+                'image_path' => 'path/to/default/2.jpg',
+                'author' => 'Penulis 2',
                 'date_taken' => '2024-11-02',
                 'location' => 'Lokasi Foto 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Foto 3',
+                'description' => 'Deskripsi foto 3', // Deskripsi bisa null
+                'image_path' => 'path/to/default/3.jpg',
+                'author' => 'Penulis 3',
+                'date_taken' => '2024-11-02',
+                'location' => 'Lokasi Foto 3',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -72,29 +72,40 @@ $com = \App\Models\Comment::whereDay('created_at','=', date('d'))->where('role_i
                 <small>{{ \Auth::user()->name }}</small>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">My Profile</span>
-                <div class="dropdown-divider"></div>
-                @if (\Auth::user()->photo == null)
-                <center><img src="https://cdn.iconscout.com/icon/free/png-512/avatar-372-456324.png" width="50%" alt="">
-                </center>
-                @else
-                <center><img src="{{ url('uploads/', \Auth::user()->photo) }}" width="50%" style="border-radius: 50%"
-                        alt="">
-                    <center>
-                        @endif
-                        <p class="text-center">{{ \Auth::user()->name }}</p>
-                        <div class="dropdown-divider"></div>
-                        <div class="row">
-                            <div class="col-6">
-                                <a href="{{ url('profile') }}" class="dropdown-item dropdown-footer"> <i
-                                        class="fa fa-list"></i> Detail</a>
-                            </div>
-                            <div class="col-6">
-                                <a href="{{ url('/keluar') }}" id="logout-btn" class="dropdown-item dropdown-footer">
-                                    <i class="fa fa-arrow-right"></i> Logout
-                                </a>
-                            </div>
+                @if (\Auth::user()->role_id == 1)
+                    <span class="dropdown-item dropdown-header">My Profile</span>
+                    <div class="dropdown-divider"></div>
+                    @if (\Auth::user()->photo == null)
+                        <center><img src="https://cdn.iconscout.com/icon/free/png-512/avatar-372-456324.png" width="50%" alt=""></center>
+                    @else
+                        <center><img src="{{ url('uploads/', \Auth::user()->photo) }}" width="50%" style="border-radius: 50%" alt=""></center>
+                    @endif
+                    <p class="text-center">{{ \Auth::user()->name }}</p>
+                    <div class="dropdown-divider"></div>
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="{{ url('profile') }}" class="dropdown-item dropdown-footer">
+                                <i class="fa fa-list"></i> Detail
+                            </a>
                         </div>
+                        <div class="col-6">
+                            <a href="{{ url('/keluar') }}" id="logout-btn" class="dropdown-item dropdown-footer">
+                                <i class="fa fa-arrow-right"></i> Logout
+                            </a>
+                        </div>
+                    </div>
+                @else
+                    @if (\Auth::user()->photo == null)
+                        <center><img src="https://cdn.iconscout.com/icon/free/png-512/avatar-372-456324.png" width="50%" alt=""></center>
+                    @else
+                        <center><img src="{{ url('uploads/', \Auth::user()->photo) }}" width="50%" style="border-radius: 50%" alt=""></center>
+                    @endif
+                    <p class="text-center mt-2">{{ \Auth::user()->name }}</p>
+                    <div class="dropdown-divider"></div>
+                    <a href="{{ url('/keluar') }}" id="logout-btn" class="dropdown-item text-center">
+                        <i class="fa fa-sign-out-alt"></i> Logout
+                    </a>
+                @endif
             </div>
         </li>
     </ul>
